53f29711397874833a4c93ed45bdc1e7e9e1a767
evycham <ygruzevich8484@gmail.com>
2026-03-12 21:06:36 +0100

    GET for already existed random jokes

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,28 @@
 'use strict';
 
 const scene = document.getElementById('joke');
+const dialogMenu = document.getElementById('dialog');
+const typeInput = document.getElementById('type');
+const jokeInput = document.getElementById('joke-input');
+const form = document.getElementById('form');
+
+const btnOpen = document.getElementById('btnOpen');
+const btnClose = document.getElementById('btnClose');
+
+btnOpen.addEventListener('click', (e) => {
+    e.preventDefault();
+    dialogMenu.showModal();
+});
+
+btnClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    dialogMenu.close();
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addJoke();
+})
 
 window.onload = getJoke;
 
@@ -19,5 +41,27 @@ async function getJoke(){
     } catch (e){
         console.error(e);
         scene.innerHTML = `Error: default`;
+    }
+}
+
+async function addJoke(){
+    let joke = {
+        type: typeInput.value,
+        value: jokeInput.value
+    }
+
+    try {
+        let response = await fetch("/epigrams", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(joke)
+        });
+        if (!response.ok) {
+            throw new Error(`Error while addJoke: ${response.statusText} (${response.status})`);
+        }
+    } catch (err) {
+        console.error(err);
     }
 }
